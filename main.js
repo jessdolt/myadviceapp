055e0cb8928753fcaa994e862bc5a373d9749b33
Your Name <you@example.com>
2020-07-27 20:45:45 +0800

    changed in button for api fetching

--- main.js
+++ main.js
@@ -5,16 +5,28 @@ btn.addEventListener('click', getAdvice);
 function getAdvice(){
     const xhr = new XMLHttpRequest();
     const url = "https://api.adviceslip.com/advice";
+
     xhr.open('GET',url, true);
     xhr.onload= function(){
         if(this.status == 200){
             let data =JSON.parse(this.responseText);
             let advice = data.slip.advice;
             console.log(advice);
-            msg.style.animation='text 0.5s 1';
+            msg.style.animation='text 1s 1';
             msg.textContent=advice;
+            btn.disabled = true;
+            btnReset();
         }
     }
     xhr.send();
     msg.style.removeProperty('animation');
+   
+}
+
+function btnReset(){
+    setInterval(function(){
+        btn.disabled = false;
+        btn.style.animation='button 0.5 1';
+    },4750)
+   
 }
